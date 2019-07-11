4e5c0c8a4033656515f2ab6ad63002324ca162d4
Hykilpikonna <admin@moecraft.cc>
2019-07-11 19:05:48 +0800

    [+] Turn on autocomplete for search

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -5,11 +5,11 @@ import {Component} from '@angular/core';
     templateUrl: 'tab2.page.html',
     styleUrls: ['tab2.page.scss']
 })
+
 export class Tab2Page
 {
-
     constructor()
     {
-    }
 
+    }
 }
