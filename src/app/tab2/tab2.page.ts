9aa8d9c79ba238ad5dbe0ece4fd9264bcddb1e5e
Hykilpikonna <admin@moecraft.cc>
2019-07-12 13:55:10 +0800

    [+] Create instance variables

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -8,8 +8,34 @@ import {Component} from '@angular/core';
 
 export class Tab2Page
 {
+    private search = document.querySelector('#hy-search-bar');
+    private itemList = document.querySelector('#hy-item-list');
+
+    public itemListArray = ['12', '3'];
+
     constructor()
     {
+        console.log("Tab2Page.constructor()");
+    }
 
+    refresh()
+    {
+        console.log("Refresh()");
+
+        this.search.addEventListener('ionInput', handleInput);
+
+        function handleInput(event)
+        {
+            const query = event.target.value.toLowerCase();
+            requestAnimationFrame(() =>
+            {
+                /*
+                items.forEach(item =>
+                {
+                    const shouldShow = item.textContent.toLowerCase().indexOf(query) > -1;
+                    item.style.display = shouldShow ? 'block' : 'none';
+                });*/
+            });
+        }
     }
 }
