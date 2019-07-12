d7fda9e3d9ed139595428b1b10d8246e7629157c
Hykilpikonna <admin@moecraft.cc>
2019-07-12 12:37:17 +0800

    [+] Import storage module in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,18 @@ import {StatusBar} from '@ionic-native/status-bar/ngx';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 
+// Storage
+import { IonicStorageModule } from '@ionic/storage';
+
 @NgModule({
     declarations: [AppComponent],
     entryComponents: [],
-    imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+    imports: [
+        BrowserModule,
+        IonicModule.forRoot(),
+        AppRoutingModule,
+        IonicStorageModule.forRoot()
+    ],
     providers: [
         StatusBar,
         SplashScreen,
