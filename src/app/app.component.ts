79f31e004b7bea82bc6a0895bcc092d085b9d998
Hykilpikonna <admin@moecraft.cc>
2019-07-14 01:10:01 +0800

    [-] Remove unused storage dependency in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,20 +3,17 @@ import {Component} from '@angular/core';
 import {Platform} from '@ionic/angular';
 import {SplashScreen} from '@ionic-native/splash-screen/ngx';
 import {StatusBar} from '@ionic-native/status-bar/ngx';
-import {Storage} from '@ionic/storage';
 
 @Component({
     selector: 'app-root',
     templateUrl: 'app.component.html'
 })
-
 export class AppComponent
 {
     constructor(
         private platform: Platform,
         private splashScreen: SplashScreen,
-        private statusBar: StatusBar,
-        private storage: Storage
+        private statusBar: StatusBar
     )
     {
         this.initializeApp();
