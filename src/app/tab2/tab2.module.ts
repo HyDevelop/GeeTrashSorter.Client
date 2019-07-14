b485384ab8e8049d3d8eeef9bb0c1d9fb2fdd96d
Hykilpikonna <admin@moecraft.cc>
2019-07-14 15:33:45 +0800

    [F] Fix "NullInjectorError: No provider for Clipboard!"

--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -4,6 +4,7 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
 import {Tab2Page} from './tab2.page';
+import {Clipboard} from '@ionic-native/clipboard/ngx';
 
 @NgModule({
     imports: [
@@ -11,6 +12,9 @@ import {Tab2Page} from './tab2.page';
         CommonModule,
         FormsModule,
         RouterModule.forChild([{path: '', component: Tab2Page}])
+    ],
+    providers: [
+        Clipboard
     ],
     declarations: [Tab2Page]
 })
