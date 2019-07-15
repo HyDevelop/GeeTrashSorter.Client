084a3a17123473cbb058f1f26b23babaecbdf18d
Hykilpikonna <admin@moecraft.cc>
2019-07-15 20:51:30 +0800

    [U] Update imports

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -3,6 +3,7 @@ import {Storage} from '@ionic/storage';
 import pWaitFor from 'p-wait-for';
 import {ActionSheetController} from '@ionic/angular';
 import {Clipboard} from '@ionic-native/clipboard/ngx';
+import {Constants} from '../constants';
 
 // TODO: Add splash
 // TODO: Add some ng-Fx
@@ -213,7 +214,7 @@ export class Tab2Page
                 target.parentNode.insertBefore(element, target.nextSibling);
             }
         };
-        request.open('GET', CORS_PROXY + BASE_URL + text, true);
+        request.open('GET', Constants.CORS_PROXY + Constants.BASE_URL + text, true);
         request.send('');
     }
 
