f249554d742f3d1dcd1b4eb5a6765f9f98b373cb
Hykilpikonna <admin@moecraft.cc>
2019-07-15 20:51:15 +0800

    [M] Separate constants class to a new file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -39,22 +39,3 @@ export class AppComponent
         });
     }
 }
-
-/**
- * Constants
- * 全局变量
- */
-export class Constants
-{
-    /**
-     * API Base URL
-     * API 基础链接 TODO: 添加北京
-     */
-    public static BASE_URL = 'http://trash.hydev.org/shanghai?name=';
-
-    /**
-     * Cross site proxy
-     * 跨站脚本代理 TODO: 写自己的代理
-     */
-    public static CORS_PROXY = 'https://cors-anywhere.herokuapp.com/';
-}
