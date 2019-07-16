268da085c6d4472042832514b2fc152e15d899c1
Hykilpikonna <admin@moecraft.cc>
2019-07-17 01:09:08 +0800

    [-] Remove baidu api access stuff from constants

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -26,39 +26,4 @@ export class Constants
      * 查询垃圾名历史
      */
     public static STORAGE_HISTORY = 'history';
-
-    /**
-     * Baidu image recognition api key information
-     * 百度图像识别 API Key
-     */
-    public static BAIDU_API_KEY_SPLIT;
-
-    /**
-     * Ensure everything is initialized
-     * 确保初始化完成
-     */
-    public static ensureInitialized(callback)
-    {
-        if (this.BAIDU_API_KEY_SPLIT == null)
-        {
-            // Get baidu image recognition api keys
-            // 获取百度识图 API Key
-            let request = new XMLHttpRequest();
-            request.onreadystatechange = function()
-            {
-                // Process results
-                // 处理结果
-                if (request.readyState == 4)
-                {
-                    let split = request.responseText.split(";");
-
-                    callback();
-                }
-                // TODO: Handle error
-            };
-            request.open('POST', Constants.CORS_PROXY + Constants.BASE_URL + "baidu-api-secret", true);
-            request.send('');
-        }
-        else callback();
-    }
 }
