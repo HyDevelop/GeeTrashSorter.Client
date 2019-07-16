1b9a1067a5d3c8709897ff46a425658477c7b0f5
Hykilpikonna <admin@moecraft.cc>
2019-07-17 01:24:40 +0800

    [F] Fix "no providers for UniqueDeviceID!"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,15 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {RouteReuseStrategy} from '@angular/router';
-
 import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
 import {SplashScreen} from '@ionic-native/splash-screen/ngx';
 import {StatusBar} from '@ionic-native/status-bar/ngx';
-
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-
 import {Camera} from '@ionic-native/camera/ngx';
 import {Base64ToGallery} from '@ionic-native/base64-to-gallery/ngx';
-
-// Storage
 import {IonicStorageModule} from '@ionic/storage';
+import {UniqueDeviceID} from '@ionic-native/unique-device-id/ngx';
 
 @NgModule({
     declarations: [AppComponent],
@@ -30,7 +26,8 @@ import {IonicStorageModule} from '@ionic/storage';
         {provide: RouteReuseStrategy, useClass: IonicRouteStrategy},
         IonicStorageModule,
         Camera,
-        Base64ToGallery
+        Base64ToGallery,
+        UniqueDeviceID
     ],
     bootstrap: [AppComponent]
 })
