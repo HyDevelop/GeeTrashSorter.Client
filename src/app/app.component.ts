da25a892bc38c33cca383f10d9b4532bf31152c3
Hykilpikonna <admin@moecraft.cc>
2019-07-17 01:08:37 +0800

    [+] Create method to initialize storeage in app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,9 @@ import {Component} from '@angular/core';
 import {Platform} from '@ionic/angular';
 import {SplashScreen} from '@ionic-native/splash-screen/ngx';
 import {StatusBar} from '@ionic-native/status-bar/ngx';
+import {UniqueDeviceID} from '@ionic-native/unique-device-id/ngx';
+import {updateBinding} from '@angular/core/src/render3/bindings';
+import {Utils} from './utils';
 
 @Component({
     selector: 'app-root',
@@ -13,10 +16,36 @@ export class AppComponent
     constructor(
         private platform: Platform,
         private splashScreen: SplashScreen,
-        private statusBar: StatusBar
+        private statusBar: StatusBar,
+        private storage: Storage,
+        private uniqueDeviceID: UniqueDeviceID
     )
     {
         this.initializeApp();
+    }
+
+    /**
+     * Initialize storage
+     * 初始化数据库
+     */
+    private initStorage()
+    {
+        // Obtain device UDID if not found
+        // 获取设备 UDID
+        // TODO: Better error handling
+        if (this.storage.getItem("info-udid") == null)
+        {
+            this.uniqueDeviceID.get().then(uuid =>
+            {
+                console.log(uuid);
+                this.storage.setItem("info-udid", uuid);
+
+            }).catch(err => console.log(err));
+
+            // Update Baidu api
+            // 初始化百度 API
+            Utils.updateBaiduApiKey(this.storage)
+        }
     }
 
     /**
