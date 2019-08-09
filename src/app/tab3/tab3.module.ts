96342ce95e6d8d8cbe9d53f8829dadd3e8dcdfb9
Hykilpikonna <admin@moecraft.cc>
2019-08-09 16:34:58 +0800

    [U] Import sui module in tab3

--- src/app/tab3/tab3.module.ts
+++ src/app/tab3/tab3.module.ts
@@ -4,13 +4,15 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
 import {Tab3Page} from './tab3.page';
+import {SuiModule} from 'ng2-semantic-ui';
 
 @NgModule({
     imports: [
         IonicModule,
         CommonModule,
         FormsModule,
-        RouterModule.forChild([{path: '', component: Tab3Page}])
+        RouterModule.forChild([{path: '', component: Tab3Page}]),
+        SuiModule
     ],
     declarations: [Tab3Page]
 })
