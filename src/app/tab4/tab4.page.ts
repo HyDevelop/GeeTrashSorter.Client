14222e969c03f1e896689bffdaafdd462a8d43e3
Hykilpikonna <admin@moecraft.cc>
2019-08-10 17:06:20 +0800

    [O] Disable view encapsulation for Tab4

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -1,9 +1,10 @@
-import {Component} from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 
 @Component({
     selector: 'app-tab4',
     templateUrl: 'tab4.page.html',
-    styleUrls: ['tab4.page.scss']
+    styleUrls: ['tab4.page.scss'],
+    encapsulation: ViewEncapsulation.None
 })
 export class Tab4Page
 {
