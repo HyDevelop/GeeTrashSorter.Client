9dfa69682621def7a6a689cf7c5acb9e928c7719
Hykilpikonna <admin@moecraft.cc>
2019-08-12 16:47:51 +0800

    [+] Add toTab function

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {TabsPage} from '../tabs/tabs.page';
+import {Tab2Page} from '../tab2/tab2.page';
 
 // TODO: Implement 提出意见
 
@@ -18,5 +19,16 @@ export class Tab1Page
     irClick()
     {
         TabsPage.getInstance().getPicture();
+    }
+
+    /**
+     * Navigate to a specific tab number
+     * 转到一个页面
+     *
+     * @param num Tab number
+     */
+    toTab(num: number)
+    {
+        TabsPage.getInstance().router.navigateByUrl("/tabs/tab" + num);
     }
 }
